75048e4c6073a7f5dd2f6d57b9237137e581daab
aldi7id <aldi7.indo@gmail.com>
2022-03-30 20:37:51 +0700

    edit MainActivity.kt

--- app/src/main/java/com/ajgroup/recyclerview/ContactAdapter.kt
+++ app/src/main/java/com/ajgroup/recyclerview/ContactAdapter.kt
@@ -8,12 +8,12 @@ import com.ajgroup.recyclerview.databinding.ActivityMainBinding
 import com.ajgroup.recyclerview.databinding.MyContactItemBinding
 
 class ContactAdapter(val listContact: ArrayList<MyContact>) : RecyclerView.Adapter<ContactAdapter.ViewHolder>() {
-    lateinit var binding: MyContactItemBinding
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    class ViewHolder(val binding: MyContactItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        binding = MyContactItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-        return ViewHolder(binding.root)
+        val binding = MyContactItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
@@ -21,7 +21,7 @@ class ContactAdapter(val listContact: ArrayList<MyContact>) : RecyclerView.Adapt
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        binding.tvName.text = listContact[position].name
-        binding.tvNoHp.text = listContact[position].noHp
+        holder.binding.tvName.text = listContact[position].name
+        holder.binding.tvNoHp.text = listContact[position].noHp
     }
 }
