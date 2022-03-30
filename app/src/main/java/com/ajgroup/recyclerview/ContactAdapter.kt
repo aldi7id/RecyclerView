3b872a33781357d816e8648765ea2e6883a8e8e5
aldi7id <aldi7.indo@gmail.com>
2022-03-30 18:14:12 +0700

    add ContactAdapter.kt

--- app/src/main/java/com/ajgroup/recyclerview/ContactAdapter.kt
+++ app/src/main/java/com/ajgroup/recyclerview/ContactAdapter.kt
@@ -1,4 +1,27 @@
 package com.ajgroup.recyclerview
 
-class ContactAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ajgroup.recyclerview.databinding.ActivityMainBinding
+import com.ajgroup.recyclerview.databinding.MyContactItemBinding
+
+class ContactAdapter(val listContact: ArrayList<MyContact>) : RecyclerView.Adapter<ContactAdapter.ViewHolder>() {
+    lateinit var binding: MyContactItemBinding
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        binding = MyContactItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding.root)
+    }
+
+    override fun getItemCount(): Int {
+        return listContact.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        binding.tvName.text = listContact[position].name
+        binding.tvNoHp.text = listContact[position].noHp
+    }
 }
